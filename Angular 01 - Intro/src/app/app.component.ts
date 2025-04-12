fc9327396f1869901fdf39a27e965e32cc156742
Rahul Lodhi <rahullodhimts@gmail.com>
2025-04-12 16:05:15 +0530

    first commit

--- Angular 01 - Intro/src/app/app.component.ts
+++ Angular 01 - Intro/src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular 01 | Intro';
+
+  myname = 'Rahul Lodhi';
+  x=20;
+  y=20;
+
+  myFunction(){
+    alert("Function call works!!");
+    this.secFunc();
+  }
+
+  secFunc(){
+    alert("Second function called!!");
+  }
+
+  mydata : string | number = 'Rahul'; //multiple data type define
+
+  myUpdate(){
+    this.mydata=45;
+    alert("Updated: "+this.mydata);
+  }
+
+  mySum(a:number,b:number){
+    alert(a+b);
+  }
+
 }
